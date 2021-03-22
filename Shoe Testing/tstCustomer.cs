16621fed164963913299b5540146dc4f7349acb1
P2507379 <P2507379@my354.dmu.ac.uk>
2021-03-22 03:54:52 +0000

    Added the find method

--- Shoe Testing/tstCustomer.cs
+++ Shoe Testing/tstCustomer.cs
@@ -29,5 +29,219 @@ namespace Shoe_Testing
             Assert.AreEqual(ACustomer.Active, TestData);
         }
 
+        [TestMethod]
+        public void CustomerIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            ACustomer.CustomerID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.CustomerID, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerFirstNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create some test data to assign to the property
+            string TestData = "First Name";
+            //assign the data to the property
+            ACustomer.CustomerFirstName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.CustomerFirstName, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerLastNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create some test data to assign to the property
+            string TestData = "Last Name";
+            //assign the data to the property
+            ACustomer.CustomerLastName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.CustomerLastName, TestData);
+        }
+        [TestMethod]
+
+        public void CustomerEmailPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create some test data to assign to the property
+            string TestData = "Some Email";
+            //assign the data to the property
+            ACustomer.CustomerEmail = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.CustomerEmail, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerTeleNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //create some test data to assign to the property
+            string TestData = "First Name";
+            //assign the data to the property
+            ACustomer.CustomerTeleNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(ACustomer.CustomerTeleNo, TestData);
+        }
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the reslt ofg the validation
+            Boolean Found = false;
+            //create sometest data to use wiht the method
+            Int32 CustomerID = 1;
+            Found = ACustomer.Find(CustomerID);
+            //test to see if the relst is true
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestCustomerIDFound()
+        {
+            //create an instance of the calss we want to create 
+            clsCustomer ACustomer = new clsCustomer();
+            //booealn variable to streot he reiult of the search 
+            Boolean Found = true;
+            //boolean variabnle to record if data is ok 
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustomerID = 1;
+            //invke teh method
+            Found = ACustomer.Find(CustomerID);
+            //chech the customer id
+            if (ACustomer.CustomerID != 1)
+            {
+                OK = false;
+            }
+            //test ito see that the result is correct
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestActiveFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //check the property
+            if (ACustomer.Active != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void TestCustomerFirstNameFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //chech the customer id
+            if (ACustomer.CustomerFirstName != "Amar")
+            {
+                OK = false;
+            }
+            //test ito see that the result is correct
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestCustomerLastNameFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //chech the customer id
+            if (ACustomer.CustomerLastName != "Sahota")
+            {
+                OK = false;
+            }
+            //test ito see that the result is correct
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestCustomerEmailFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //chech the customer id
+            if (ACustomer.CustomerEmail != "amar.sahota@gmail.com")
+            {
+                OK = false;
+            }
+            //test ito see that the result is correct
+            Assert.IsTrue(OK);
+
+        }
+
+        [TestMethod]
+        public void TestCustomerTeleNoFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 CustomerID = 1;
+            //invoke the method
+            Found = ACustomer.Find(CustomerID);
+            //chech the customer id
+            if (ACustomer.CustomerTeleNo != "07334123123")
+            {
+                OK = false;
+            }
+            //test ito see that the result is correct
+            Assert.IsTrue(OK);
+
+        }
     }
 }
--- ShoeClasses/clsCustomer.cs
+++ ShoeClasses/clsCustomer.cs
@@ -1,7 +1,140 @@
-﻿namespace ShoeClasses
+﻿using System;
+
+namespace ShoeClasses
 {
     public class clsCustomer
     {
-        public bool Active { get; set; }
+
+        //private data member for the cutsomer id property
+        private Int32 mCustomerID;
+        //CustomerID public property
+        public Int32 CustomerID
+        {
+            get
+            {
+                //this lione of code sends data out of the property
+                return mCustomerID;
+            }
+            set
+            {
+                //this line od code allows data into the property
+                mCustomerID = value;
+            }
+        }
+
+        //private data member for active
+        private Boolean mActive;
+        //public property for active
+        public bool Active
+        {
+            get
+            {
+                //return the private data
+                return mActive;
+            }
+            set
+            {
+                //set the private data
+                mActive = value;
+            }
+        }
+
+        //private data member for first name
+        private string mCustomerFirstName;
+        //public property for first name
+        public string CustomerFirstName
+        {
+            get
+            {
+                //return the private data
+                return mCustomerFirstName;
+            }
+            set
+            {
+                //set the private data
+                mCustomerFirstName = value;
+            }
+        }
+
+        //private data member for last name
+        private string mCustomerLastName;
+        //public property for last name
+        public string CustomerLastName
+        {
+            get
+            {
+                //return the private data
+                return mCustomerLastName;
+            }
+            set
+            {
+                //set the private data
+                mCustomerLastName = value;
+            }
+        }
+
+        //private data member for email
+        private string mCustomerEmail;
+        //public property for email
+        public string CustomerEmail
+        {
+            get
+            {
+                //return the private data
+                return mCustomerEmail;
+            }
+            set
+            {
+                //set the private data
+                mCustomerEmail = value;
+            }
+        }
+
+        //private data member for tele no
+        private string mCustomerTeleNo;
+        //public property for tele no
+        public string CustomerTeleNo
+        {
+            get
+            {
+                //return the private data
+                return mCustomerTeleNo;
+            }
+            set
+            {
+                //set the private data
+                mCustomerTeleNo = value;
+            }
+        }
+
+
+        public bool Find(int CustomerID)
+        {
+            //initialise the DBConnection
+            clsDataConnection dBConnection = new clsDataConnection();
+            //add the device no parameter
+            dBConnection.AddParameter("@CustomerID", CustomerID);
+            //execute the query
+            dBConnection.Execute("sproc_tblCustomer_FilterByCustomerID");
+            //if the record was found
+            if (dBConnection.Count == 1)
+            {
+                //get the device no.
+                mCustomerID = Convert.ToInt32(dBConnection.DataTable.Rows[0]["CustomerID"]);
+                mCustomerFirstName = Convert.ToString(dBConnection.DataTable.Rows[0]["CustomerFirstName"]);
+                mCustomerLastName = Convert.ToString(dBConnection.DataTable.Rows[0]["CustomerLastName"]);
+                mCustomerEmail = Convert.ToString(dBConnection.DataTable.Rows[0]["CustomerEmail"]);
+                mCustomerTeleNo = Convert.ToString(dBConnection.DataTable.Rows[0]["CustomerTeleNo"]);
+                mActive = Convert.ToBoolean(dBConnection.DataTable.Rows[0]["Active"]);
+                return true;
+            }
+            //if no record was found
+            else
+            {
+            //return indication a problem 
+            return false;
+            }
+            
+        }
     }
 }
